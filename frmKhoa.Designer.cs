315b2601be8f89d74cb05c294c24b380b29e00b2
Admin <Admin@DESKTOP-CKJRV3Q>
2025-06-24 22:29:44 +0700

    2

--- frmKhoa.Designer.cs
+++ frmKhoa.Designer.cs
@@ -31,6 +31,9 @@
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
             SuspendLayout();
             // 
             // label1
@@ -60,11 +63,35 @@
             label3.TabIndex = 2;
             label3.Text = "Tên khoa";
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(163, 22);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(179, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(163, 129);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(179, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(163, 76);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(179, 23);
+            textBox3.TabIndex = 5;
+            // 
             // frmKhoa
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(815, 395);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -79,5 +106,8 @@
         private Label label1;
         private Label label2;
         private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
     }
 }
